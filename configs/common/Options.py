db1b95be4d461d6027aac7d69b9669526ce13387
pengxiao <384756158@qq.com>
2024-12-11 17:48:31 +0800

    cpu-o3: fx `--disable-sc` help msg
    Change-Id: I1fa6e1496d814802676cd500e3a5b8f0f71519bb

--- configs/common/Options.py
+++ configs/common/Options.py
@@ -270,7 +270,7 @@ def addCommonOptions(parser, configure_xiangshan=False):
                         "basic branch trace is enabled by default even without specifying, "
                         "available subdatabase: basic, tage, ras, loop")
     parser.add_argument("--disable-sc", default=False, action="store_true",
-                        help="enable sc (only for ftb branch predictor)")
+                        help="disable SC (enabled by default, only for FTBTAGE)")
     parser.add_argument("--enable-loop-buffer", default=False, action="store_true",
                         help="enable loop buffer (only for ftb branch predictor)")
     parser.add_argument("--enable-loop-predictor", default=False, action="store_true",
